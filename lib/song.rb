c5aa44d272e9062d6ceaad5f1f4f95f5aeb0d860
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-18 19:41:11 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,9 +9,11 @@ class Song
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
-    @@artists << artist
     @genre = genre
+    
+    @@artists << artist
     @@genres << genre
+    
     @@count += 1
   end
   
