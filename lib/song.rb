53b217f4dea2309cc928aed11f801e81422d3b32
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-18 19:35:52 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,15 +6,7 @@ class Song
     @@count
   end 
   
-  # Control for duplicates when you code your 
-  # .genres class method, not when you add 
-  # genres to the original @@genres array.
   @@genres = []
-  
-# this should be deleted at some point
-# def self.total_genre_entries
-#   @@genres
-# end
   
   def self.genres
     @@genres.uniq
