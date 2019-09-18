89efcba6aae36973025e257c1a468261b67cf60f
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-18 19:39:25 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,18 @@ class Song
     @@artists.inject(Hash.new(0)) { |total, i| total[i] += 1 ;total}
   end
   
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+        artist_count[artist] += 1 
+      else
+        artist_count[artist] = 1
+      end
+    end
+    artist_count
+  end
+  
   def self.artists
     @@artists.uniq
   end
